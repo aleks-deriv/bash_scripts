ecd6896be29d9b7838e423ed16a21bd98d2a185d
aleks-deriv <114407493+aleks-deriv@users.noreply.github.com>
2022-10-11 13:46:49 +0300

    Update vpn_killswitch_linux.sh

--- vpn_killswitch_linux.sh
+++ vpn_killswitch_linux.sh
@@ -5,11 +5,12 @@ pip3 install requests || true
 py_loc=`which python3`
 
 cat <<EOT > /opt/vpn_killswitch.py
-#!/usr/bin/python3
+#!${py_loc}
 import requests
 import os, platform, sys, datetime
 import json, getpass
 import subprocess
+import netifaces
 from uuid import getnode as get_mac
 api_url = "http://vpnmonitoring.deriv.cloud/ipcheck"
 api_url_notify = "http://vpnmonitoring.deriv.cloud/notify"
@@ -21,13 +22,13 @@ if sys.platform.startswith('win32'):
 	username = ""
 	wifi_ssid = ""
 elif sys.platform.startswith('linux'):
-	username = subprocess.check_output("echo ", shell=True).rstrip()
-	wifi_ssid = subprocess.check_output("iw dev | grep ssid | awk {'for (i=2; i<=NF; i++) printf\"%s \",$i'}", shell=True).rstrip().decode('utf-8')
+	username = subprocess.check_output("echo $SUDO_USER", shell=True).rstrip()
+	wifi_ssid = subprocess.check_output("iw dev | grep ssid | awk {'for (i=2; i<=NF; i++) printf\"%s \",\$i'}", shell=True).rstrip().decode('utf-8')
 	if wifi_ssid == "":
-		wifi_ssid = subprocess.check_output("nmcli --fields IN-USE,SSID device wifi | grep  \* | awk 'BEGIN {IGNORECASE = 1} !/SSID/' | awk '{for (i=2; i<NF; i++) printf $i \" \"; print $NF}' | awk 'NR == 1'", shell=True).rstrip().decode('utf-8')
+		wifi_ssid = subprocess.check_output("nmcli --fields IN-USE,SSID device wifi | grep  \* | awk 'BEGIN {IGNORECASE = 1} !/SSID/' | awk '{for (i=2; i<NF; i++) printf \$i \" \"; print \$NF}' | awk 'NR == 1'", shell=True).rstrip().decode('utf-8')
 elif sys.platform.startswith('darwin'):
 	username = subprocess.check_output("users", shell=True).rstrip()
-	wifi_ssid = subprocess.check_output("/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F: '/ SSID/{print $2}'", shell=True).rstrip()
+	wifi_ssid = subprocess.check_output("/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F: '/ SSID/{print \$2}'", shell=True).rstrip()
 	wifi_ssid = wifi_ssid.strip().decode("utf-8").replace('SSID: ', '')
 	
 if wifi_ssid == "RMGTech 5GHz" :
@@ -48,7 +49,7 @@ x = {
   "os": platform.system(), 
   "name": username,
   "wifi_ssid" : wifi_ssid,
-  "mac_add" : get_mac(),
+  "mac_add" : netifaces.ifaddresses('enp4s0')[17][0]['addr'], # get_mac(),
   "office" : office
 }
 try:
@@ -75,10 +76,8 @@ if (country_code == "BY" or country_code == "IR" ) :
 		interface = subprocess.check_output(cmd, shell=True).rstrip()
 		cmd = "networksetup -removepreferredwirelessnetwork " + interface.decode("utf-8") + " " + wifi_ssid			
 		os.system(cmd)
-		cmd = 'networksetup -listallnetworkservices | while read -r line; do networksetup -setnetworkserviceenabled "$line" off; done'
+		cmd = 'networksetup -listallnetworkservices | while read -r line; do networksetup -setnetworkserviceenabled "\$line" off; done'
 		os.system(cmd)
-
-
 EOT
 
 runscript=`chmod +x /opt/vpn_killswitch.py`
@@ -93,3 +92,5 @@ else
 fi
 
 exit 0
+
+
